aeb72beff9bcff48c76e26e99b7652bd18287bcf
Work <work@Nicholass-MacBook-Air.local>
2023-12-15 16:44:55 -0600

    first

---
+++ make_model.py
@@ -0,0 +1,28 @@
+import torch
+import os
+import shutil
+from ultralytics import YOLO
+import time
+import dingpy
+
+
+import time
+
+
+
+sound_file = '/Users/work/Documents/oven.mp3'
+
+start_time = time.time()
+
+model = YOLO('yolov8n.pt') # Nano model
+model.to('mps')
+model.train(data='config.yaml', epochs=5, device='mps')
+
+end_time = time.time()
+
+# Calculate the elapsed time
+print()
+print('Time:', (end_time - start_time) /  3600 ) 
+
+
+dingpy.ding(path = '/Users/work/Documents/oven.mp3' )
